c9c533cee96205cfe7ce647882a489fa538302e2
felipeam10 <felipeam10@hotmail.com>
2021-03-21 12:25:04 -0300

    exemplo de projecoes

--- src/main/java/br/com/felipe/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/felipe/spring/data/repository/FuncionarioRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.felipe.spring.data.orm.Funcionario;
+import br.com.felipe.spring.data.orm.FuncionarioProjecao;
 
 @Repository
 public interface FuncionarioRepository extends PagingAndSortingRepository<Funcionario, Integer> { //PagingAndSortingRepository eh quem faz a paginação no spring
@@ -19,4 +20,7 @@ public interface FuncionarioRepository extends PagingAndSortingRepository<Funcio
 	
 	@Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
 	List<Funcionario> findDataContratacaoMaior(LocalDate data);
+	
+	@Query(value = "select f.id, f.nome, f.salario from funcionarios f", nativeQuery = true)
+	List<FuncionarioProjecao> findFuncionarioSalario();
 }
